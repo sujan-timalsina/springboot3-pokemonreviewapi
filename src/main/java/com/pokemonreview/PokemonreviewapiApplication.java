f86b978133b9dedd93ec8ce6c76e5588b781d3a8
sujan-timalsina <sujan.timalsina@apexcollege.edu.np>
2022-12-24 12:54:46 +0545

    worked on models

---
+++ src/main/java/com/pokemonreview/PokemonreviewapiApplication.java
@@ -0,0 +1,13 @@
+package com.pokemonreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonreviewapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonreviewapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pokemonreview/PokemonreviewapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokemonreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonreviewapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
